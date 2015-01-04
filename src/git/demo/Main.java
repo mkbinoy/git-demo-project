f6b2d4065457a1b089d627ada7e0e366e343768c
Binoy M.K <binoyfisat@gmail.com>
2015-01-04 12:20:04 +0100

    Update Main.java

--- src/git/demo/Main.java
+++ src/git/demo/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	 * @param args 
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Giasdasdt");
+		System.out.println("Hello GIT");
 	}
 	
 	private void sayHello(){
