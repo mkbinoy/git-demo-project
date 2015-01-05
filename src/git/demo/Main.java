ae769a216bda5eef7e818cf8791f431fa9fe0e3a
Binoy <binoy.mk@in.bosch.com>
2015-01-05 11:55:59 +0100

    commit 5.1

--- src/git/demo/Main.java
+++ src/git/demo/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	 * @param args 
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello GIT");
+		System.out.println("Hello GIT version2");
 	}
 	
 	private void sayHello(){
